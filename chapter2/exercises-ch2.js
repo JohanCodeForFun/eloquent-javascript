902860e10512cc85f87c23ef5218a7fd0f1376c3
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-04-20 16:43:15 +0200

    first shitty draft to make seven calls

---
+++ chapter2/exercises-ch2.js
@@ -0,0 +1,23 @@
+/*
+Exercises:
+- Looping a triangle
+- FizzBuzz
+*/
+
+/* Write a loop that makes seven calls to console.log to output the following triangle
+
+#
+##
+###
+####
+#####
+######
+#######
+
+*/
+
+const calls = 7;
+
+for (let i = 1; i <= calls; i++) {
+  console.log("#", i)
+}
