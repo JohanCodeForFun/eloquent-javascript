2b0d39cd144eb88de984b545eda12a326f1e8ddf
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-04-20 21:11:33 +0200

    simplify logic and fix spelling

--- chapter2/exercises-ch2.js
+++ chapter2/exercises-ch2.js
@@ -16,42 +16,54 @@ Exercises:
 
 */
 
+console.log("------ Triangle ------\n")
+
 const calls = 7;
 let string = "";
 
 for (let i = 1; i <= calls; i++) {
   string += "#"
 
-  // console.log(string)
+  console.log(string)
 }
+
+console.log("\n")
+console.log("--- End of exercise ---\n")
+console.log("------ FizzBuzz ------\n")
 
 /*
 FizzBuzz
 
-Write all numbers  form 1 to 100
-numbers divisilb by 3, print "Fuzz"
-numbers divisilb by 5, print "Buzz"
+Write all numbers from 1 to 100
+numbers divisible by 3, print "Fuzz"
+numbers divisible by 5, print "Buzz"
 numbers both divisilb by 3 & 5, print "FizzBuzz"
 */
 
 let number = 100;
+const FIZZ = "Fizz";
+const BUZZ = "Buzz";
+const FIZZBUZZ = FIZZ + BUZZ;
 
-/*
-for (let i = 1; number >= i; i++) {
-  if ((i % 3 == 0) && (i % 5 == 0)) {
-    console.log("FizzBuzz")
+
+for (let i = 1; i <= number; i++) {
+  
+  if (i % 15 == 0) {
+    // least common multiple (LCM), 15
+    console.log(FIZZBUZZ)
   } else if (i % 3 == 0) {
-    console.log("Fizz")
+    console.log(FIZZ)
   } else if (i % 5 == 0) {
-    console.log("Buzz")
+    console.log(BUZZ)
   }
   else {
     console.log(i)
   }
 }
 
-*/
-
+console.log("\n")
+console.log("------ End of buzz ------\n")
+console.log("------ Chessboard ------\n")
 // Chess
 
 let chessBoard = ""
