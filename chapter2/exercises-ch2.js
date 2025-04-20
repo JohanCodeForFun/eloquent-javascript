359635a1903d6b77eb5715efd55b4a104c13a20a
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-04-20 16:49:03 +0200

    log number 1 to 100

--- chapter2/exercises-ch2.js
+++ chapter2/exercises-ch2.js
@@ -21,6 +21,21 @@ let string = "";
 
 for (let i = 1; i <= calls; i++) {
   string += "#"
-  
-  console.log(string)
+
+  // console.log(string)
+}
+
+/*
+FizzBuzz
+
+Write all numbers  form 1 to 100
+numbers divisilb by 3, print "Fuzz"
+numbers divisilb by 5, print "Buzz"
+numbers both divisilb by 3 & 5, print "FizzBuzz"
+*/
+
+let number = 100;
+
+for (let i = 1; number >= i; i++) {
+  console.log(i)
 }
