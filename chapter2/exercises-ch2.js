5e91c09a441cd5f2f470a54407f4ac9245afdc72
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-04-20 16:57:16 +0200

    bugged fizz buzz

--- chapter2/exercises-ch2.js
+++ chapter2/exercises-ch2.js
@@ -37,5 +37,17 @@ numbers both divisilb by 3 & 5, print "FizzBuzz"
 let number = 100;
 
 for (let i = 1; number >= i; i++) {
-  console.log(i)
+  if (i % 3 == 0) {
+    console.log("Fizz", i, i / 3)
+  } else if (i % 5 == 0) {
+    console.log("Buzz", i, i / 5)
+  } else if ((i % 3 == 0) && (i % 5 == 0)) {
+    console.log("FizzBuzz")
+  }
+  
+  else {
+    console.log(i)
+  }
+  // console.log(i, i % 3 == 0)
+  // if (i / 3 )
 }
