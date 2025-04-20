8e2a515bde5c3f86f16fdb7551b5b28286aaadb5
JohanCodeForFun <hellberg.johan@hotmail.com>
2025-04-20 16:59:47 +0200

    FizzBuzz done

--- chapter2/exercises-ch2.js
+++ chapter2/exercises-ch2.js
@@ -37,17 +37,16 @@ numbers both divisilb by 3 & 5, print "FizzBuzz"
 let number = 100;
 
 for (let i = 1; number >= i; i++) {
-  if (i % 3 == 0) {
-    console.log("Fizz", i, i / 3)
-  } else if (i % 5 == 0) {
-    console.log("Buzz", i, i / 5)
-  } else if ((i % 3 == 0) && (i % 5 == 0)) {
+  if ((i % 3 == 0) && (i % 5 == 0)) {
     console.log("FizzBuzz")
+  } else if (i % 3 == 0) {
+    console.log("Fizz")
+  } else if (i % 5 == 0) {
+    console.log("Buzz")
   }
-  
   else {
     console.log(i)
   }
-  // console.log(i, i % 3 == 0)
-  // if (i / 3 )
 }
+
+
